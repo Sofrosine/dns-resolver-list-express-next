8ecf08962cf6725acef16db983cf33685a37eb4f
Sofrosine <soultanmuh@gmail.com>
2024-11-12 19:28:29 +0700

    fix script

--- server/scripts/ensure-db-exists.sh
+++ server/scripts/ensure-db-exists.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if docker exec coldsire-coldsire_test_db-1 psql -U postgres -w -lqt | cut -d \| -f 1 | grep -qw "$PGDATABASE"; then
+if docker exec dns-resolver-list-express-next-coldsire_test_db-1 psql -U postgres -w -lqt | cut -d \| -f 1 | grep -qw "$PGDATABASE"; then
   echo "Database $PGDATABASE exists"
 else
-  docker exec coldsire-coldsire_test_db-1 psql -U postgres -w -c "CREATE DATABASE $PGDATABASE"
+  docker exec dns-resolver-list-express-next-coldsire_test_db-1 psql -U postgres -w -c "CREATE DATABASE $PGDATABASE"
   echo "Created database $PGDATABASE"
 fi
 
 # Run the CREATE TABLE command within the specified database
-docker exec coldsire-coldsire_test_db-1 psql -U postgres -w -d "$PGDATABASE" -c "
+docker exec dns-resolver-list-express-next-coldsire_test_db-1 psql -U postgres -w -d "$PGDATABASE" -c "
 CREATE TABLE IF NOT EXISTS domains (
     id SERIAL PRIMARY KEY,
     domain_name VARCHAR(255) NOT NULL UNIQUE,
